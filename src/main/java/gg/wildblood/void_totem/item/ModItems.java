4e48bf8c17115bc69c34e8dffb0eaae0a2368958
Gianni Bischoff <Oucori@gmail.com>
2024-07-03 20:48:41 +0200

    Minor Refactor

--- src/main/java/gg/wildblood/void_totem/item/ModItems.java
+++ src/main/java/gg/wildblood/void_totem/item/ModItems.java
@@ -1,7 +1,5 @@
 package gg.wildblood.void_totem.item;
 
-import gg.wildblood.void_totem.item.custom.VoidTotemItem;
-import gg.wildblood.void_totem.multiplayer.VoidTotemNetworkingConstants;
 import net.minecraft.item.Item;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
@@ -9,7 +7,6 @@ import net.minecraft.util.Identifier;
 import net.minecraft.util.Rarity;
 import org.quiltmc.loader.api.ModContainer;
 import org.quiltmc.qsl.item.setting.api.QuiltItemSettings;
-import org.quiltmc.qsl.networking.api.client.ClientPlayNetworking;
 
 public class ModItems {
 	public static final Item VOID_TOTEM = new Item(new QuiltItemSettings().rarity(Rarity.UNCOMMON).maxCount(1));
