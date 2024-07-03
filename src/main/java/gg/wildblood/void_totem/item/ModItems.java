405041cf4c28a90accfc878837548a7d3705f54f
Gianni Bischoff <Oucori@gmail.com>
2024-07-03 21:09:56 +0200

    Added the Void Totem to the Combat Item Group

--- src/main/java/gg/wildblood/void_totem/item/ModItems.java
+++ src/main/java/gg/wildblood/void_totem/item/ModItems.java
@@ -1,6 +1,10 @@
 package gg.wildblood.void_totem.item;
 
-import net.minecraft.item.Item;
+import gg.wildblood.void_totem.item.custom.VoidTotemItem;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.item.Items;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
@@ -9,10 +13,11 @@ import org.quiltmc.loader.api.ModContainer;
 import org.quiltmc.qsl.item.setting.api.QuiltItemSettings;
 
 public class ModItems {
-	public static final Item VOID_TOTEM = new Item(new QuiltItemSettings().rarity(Rarity.UNCOMMON).maxCount(1));
+	public static final VoidTotemItem VOID_TOTEM = new VoidTotemItem(new QuiltItemSettings().rarity(Rarity.UNCOMMON).maxCount(1));
 
 	public static void register(ModContainer mod) {
 		Registry.register(Registries.ITEM, new Identifier(mod.metadata().id(), "void_totem"), VOID_TOTEM);
+		ItemGroupEvents.modifyEntriesEvent(ItemGroups.COMBAT).register((group) -> group.addItem(VOID_TOTEM));
 	}
 
 }
